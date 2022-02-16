1211414b79a2786b1d31801c1a9b07dfb7845695
fabio <fabiojuvenalpereira@hotmail.com>
2022-02-16 12:15:10 -0300

    cria arquivo de validação

--- backend/src/services/task.services.js
+++ backend/src/services/task.services.js
@@ -1,9 +1,15 @@
-const taskModel = require('../models/task.model');
+// const taskModel = require('../models/task.model');
+const { validateTaskEntries } = require('./utils');
 
 const createTask = async (userName, taskContent, date, status) => {
   const taskUser =  { userName, taskContent, date, status };
-  const createdTask = await taskModel.createTask(taskUser)
-  return { status: 200, content: createdTask }
+  
+  const notIsValid = await validateTaskEntries(taskUser);
+  if (notIsValid) return { status: notIsValid.status, content: { message: notIsValid.message }}
+
+  
+  // const createdTask = await taskModel.createTask(taskUser)
+  return { status: 200, content: 'createdTask' }
 };
 
 module.exports = {
---
+++ backend/src/services/utils.js
@@ -0,0 +1,29 @@
+const BaseJoi = require('joi');
+const JoiDate = require('@hapi/joi-date');
+const Joi = BaseJoi.extend(JoiDate);
+
+const dateFormat = `${ Date.now }`;
+
+const SCHEMA = Joi.object ({
+  userName: Joi.string().required(),
+  taskContent: Joi.string().required(),
+  date: Joi.date().format(dateFormat).required().error( new Error('Invalid Date')),
+  status: Joi.string().required(),
+});
+
+
+const validateTaskEntries =  async ({ userName, taskContent, date, status }) => {
+  const isValid = SCHEMA.validate({ userName, taskContent, date, status });
+  if (isValid.error) {
+    return {
+      status: 400,
+      message: isValid.error.message
+    }
+  }
+
+}
+
+
+module.exports = {
+  validateTaskEntries,
+};
