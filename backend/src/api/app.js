8ad9700d2b9f80ec06bf485c7effb4a1d21b1220
fabio <fabiojuvenalpereira@hotmail.com>
2022-02-17 00:01:28 -0300

    configura cors

--- backend/src/api/app.js
+++ backend/src/api/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const Routes = require('../routes/index.routes');
+const cors = require('cors');
 
 const app = express();
+const PERMITED_URL = process.env.APLICATION_URL || 'http://localhost:3000/'
+
+const CORS_OPTIONS = {
+  origin: PERMITED_URL,
+  methods: ['POST', 'GET'],
+}
+
+app.use(cors(CORS_OPTIONS));
 
 app.use(express.json());
 
